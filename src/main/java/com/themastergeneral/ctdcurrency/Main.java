237f11281b29ceb806f89b80fb61eefec514743a
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2016-10-27 14:43:37 -0400

    1.1.0

--- src/main/java/com/themastergeneral/ctdcurrency/Main.java
+++ src/main/java/com/themastergeneral/ctdcurrency/Main.java
@@ -1,5 +1,8 @@
 package com.themastergeneral.ctdcurrency;
 
+import net.minecraftforge.common.MinecraftForge;
+
+import com.themastergeneral.ctdcurrency.events.Events;
 import com.themastergeneral.ctdcurrency.proxy.CommonProxy;
 
 import cpw.mods.fml.common.Mod;
@@ -16,7 +19,7 @@ public class Main
 
     public static final String MODID = "ctdcurrency";
     public static final String MODNAME = "CTD Currency";
-    public static final String VERSION = "1.0.0c";
+    public static final String VERSION = "1.1.0";
         
     @Instance
     public static Main instance = new Main();
@@ -29,6 +32,7 @@ public class Main
     {
     	System.out.println("CTD Currency loading...");
     	proxy.preInit(e);
+    	MinecraftForge.EVENT_BUS.register(new Events());
     }
         
     @EventHandler
