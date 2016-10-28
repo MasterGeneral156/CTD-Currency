deff92e953cdb8b150cd7d9229c42754832baa08
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2016-10-28 10:18:12 -0400

    Added achievements! Yay!

--- src/main/java/com/themastergeneral/ctdcurrency/events/Events.java
+++ src/main/java/com/themastergeneral/ctdcurrency/events/Events.java
@@ -2,6 +2,7 @@ package com.themastergeneral.ctdcurrency.events;
 
 import java.util.Random;
 
+import com.themastergeneral.ctdcurrency.events.achievements.Achievements;
 import com.themastergeneral.ctdcurrency.items.ModItems;
 
 import net.minecraft.entity.boss.EntityDragon;
@@ -12,6 +13,7 @@ import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent.ItemPickupEvent;
 
 public class Events 
 {
@@ -39,4 +41,64 @@ public class Events
 	    	  }
 	      }
 	}
+	@SubscribeEvent
+	 public void pickup(ItemPickupEvent event)
+	 {
+		if(event.pickedUp.getEntityItem().getItem() == ModItems.hundredth)
+		{
+			event.player.addStat(Achievements.Money1, 1);
+		}
+		if(event.pickedUp.getEntityItem().getItem() == ModItems.twenthith)
+		{
+			event.player.addStat(Achievements.Money2, 1);
+		}
+		if(event.pickedUp.getEntityItem().getItem() == ModItems.tenth)
+		{
+			event.player.addStat(Achievements.Money3, 1);
+		}
+		if(event.pickedUp.getEntityItem().getItem() == ModItems.quarter)
+		{
+			event.player.addStat(Achievements.Money4, 1);
+		}
+		if(event.pickedUp.getEntityItem().getItem() == ModItems.one)
+		{
+			event.player.addStat(Achievements.Money5, 1);
+		}
+		if(event.pickedUp.getEntityItem().getItem() == ModItems.five)
+		{
+			event.player.addStat(Achievements.Money6, 1);
+		}
+		if(event.pickedUp.getEntityItem().getItem() == ModItems.ten)
+		{
+			event.player.addStat(Achievements.Money7, 1);
+		}
+		if(event.pickedUp.getEntityItem().getItem() == ModItems.twenty)
+		{
+			event.player.addStat(Achievements.Money8, 1);
+		}
+		if(event.pickedUp.getEntityItem().getItem() == ModItems.fifty)
+		{
+			event.player.addStat(Achievements.Money9, 1);
+		}
+		if(event.pickedUp.getEntityItem().getItem() == ModItems.hundred)
+		{
+			event.player.addStat(Achievements.Money10, 1);
+		}
+		if(event.pickedUp.getEntityItem().getItem() == ModItems.thousand)
+		{
+			event.player.addStat(Achievements.Money11, 1);
+		}
+		if(event.pickedUp.getEntityItem().getItem() == ModItems.tenthousand)
+		{
+			event.player.addStat(Achievements.Money12, 1);
+		}
+		if(event.pickedUp.getEntityItem().getItem() == ModItems.hundredthousand)
+		{
+			event.player.addStat(Achievements.Money13, 1);
+		}
+		if(event.pickedUp.getEntityItem().getItem() == ModItems.million)
+		{
+			event.player.addStat(Achievements.Money14, 1);
+		}
+	 }
 }
--- src/main/java/com/themastergeneral/ctdcurrency/proxy/CommonProxy.java
+++ src/main/java/com/themastergeneral/ctdcurrency/proxy/CommonProxy.java
@@ -1,15 +1,23 @@
 package com.themastergeneral.ctdcurrency.proxy;
 
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
+import com.themastergeneral.ctdcurrency.events.Events;
+import com.themastergeneral.ctdcurrency.events.achievements.Achievements;
 import com.themastergeneral.ctdcurrency.items.ModItems;
 
 public class CommonProxy {
 
     public void preInit(FMLPreInitializationEvent e) {
     	ModItems.init();
+    	Achievements.init();
+    	Achievements.secondinit();
+    	MinecraftForge.EVENT_BUS.register(new Events());
+    	FMLCommonHandler.instance().bus().register(new Events());
     }
 
     public void init(FMLInitializationEvent e) {
