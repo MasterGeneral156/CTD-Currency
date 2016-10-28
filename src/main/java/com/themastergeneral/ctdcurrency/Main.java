b7a18838906958af3e86a05441f45bb8c4ab3bb9
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2016-10-28 10:49:52 -0400

    Language fixes.

--- src/main/java/com/themastergeneral/ctdcurrency/Main.java
+++ src/main/java/com/themastergeneral/ctdcurrency/Main.java
@@ -20,7 +20,7 @@ public class Main
 
     public static final String MODID = "ctdcurrency";
     public static final String MODNAME = "CTD Currency";
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.2.1";
         
     @Instance
     public static Main instance = new Main();
