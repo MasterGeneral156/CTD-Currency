ac298c66dc7ac804b8f5a1b5672c33482714fd12
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2016-12-15 11:07:09 -0500

    Add files via upload

--- src/main/java/com/themastergeneral/ctdcurrency/handler/Crafting.java
+++ src/main/java/com/themastergeneral/ctdcurrency/handler/Crafting.java
@@ -13,6 +13,33 @@ public class Crafting
 	}
 	public static void ConvertUp()
 	{
-		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.twenthith, 1), new ItemStack(ModItems.hundredth, 5));
+		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.twenthith, 1),
+				new ItemStack(ModItems.hundredth), 
+				new ItemStack(ModItems.hundredth), 
+				new ItemStack(ModItems.hundredth), 
+				new ItemStack(ModItems.hundredth), 
+				new ItemStack(ModItems.hundredth));
+		
+		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.tenth, 1), 	
+				new ItemStack(ModItems.twenthith), 
+				new ItemStack(ModItems.twenthith));
+		
+		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.quarter, 1), 	
+				new ItemStack(ModItems.twenthith),
+				new ItemStack(ModItems.twenthith),
+				new ItemStack(ModItems.twenthith),
+				new ItemStack(ModItems.twenthith),
+				new ItemStack(ModItems.twenthith));
+		
+		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.quarter, 1), 	
+				new ItemStack(ModItems.tenth),
+				new ItemStack(ModItems.tenth),
+				new ItemStack(ModItems.twenthith));
+		
+		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.one, 1), 	
+				new ItemStack(ModItems.quarter),
+				new ItemStack(ModItems.quarter),
+				new ItemStack(ModItems.quarter),
+				new ItemStack(ModItems.quarter));
 	}
 }
