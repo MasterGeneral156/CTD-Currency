4b0c845b6acd84a66929be87b8921e68d59cec22
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2016-12-15 11:44:07 -0500

    Updated item render register

--- src/main/java/com/themastergeneral/ctdcurrency/client/render/items/ItemRenderRegister.java
+++ src/main/java/com/themastergeneral/ctdcurrency/client/render/items/ItemRenderRegister.java
@@ -7,29 +7,10 @@ import net.minecraft.item.Item;
 import com.themastergeneral.ctdcurrency.Main;
 import com.themastergeneral.ctdcurrency.items.ModItems;
 
-public final class ItemRenderRegister {
-
-	public static String modid = Main.MODID;
-
-	public static void registerItemRenderer() {
-	    reg(ModItems.hundredth);
-	    reg(ModItems.twenthith);
-	    reg(ModItems.tenth);
-	    reg(ModItems.quarter);
-	    reg(ModItems.one);
-	    reg(ModItems.five);
-	    reg(ModItems.ten);
-	    reg(ModItems.twenty);
-	    reg(ModItems.fifty);
-	    reg(ModItems.hundred);
-	    reg(ModItems.thousand);
-	    reg(ModItems.tenthousand);
-	    reg(ModItems.hundredthousand);
-	    reg(ModItems.million);
-	}
-
-	public static void reg(Item item) {
-	    Minecraft.getMinecraft().getRenderItem().getItemModelMesher()
-	    .register(item, 0, new ModelResourceLocation(modid + ":" + item.getUnlocalizedName().substring(5), "inventory"));
+public final class ItemRenderRegister 
+{
+	public interface ItemModelProvider 
+	{
+		void registerItemModel(Item item);
 	}
 }
