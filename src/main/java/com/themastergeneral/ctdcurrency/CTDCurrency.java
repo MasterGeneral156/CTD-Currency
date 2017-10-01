3b0da73686302e6c479fe4d7b749847eb3a6a991
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-09-30 20:56:51 -0400

    Jar signed mod, added logging

--- src/main/java/com/themastergeneral/ctdcurrency/CTDCurrency.java
+++ src/main/java/com/themastergeneral/ctdcurrency/CTDCurrency.java
@@ -1,9 +1,11 @@
 package com.themastergeneral.ctdcurrency;
 
+import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -12,15 +14,16 @@ import org.apache.logging.log4j.Logger;
 
 import com.themastergeneral.ctdcurrency.proxy.CommonProxy;
 
-@Mod(modid = CTDCurrency.MODID, name = CTDCurrency.MODNAME, version = CTDCurrency.VERSION, dependencies = CTDCurrency.DEPENDENCIES, updateJSON = CTDCurrency.updateJSON, acceptedMinecraftVersions = CTDCurrency.MCVersion)
+@Mod(modid = CTDCurrency.MODID, name = CTDCurrency.MODNAME, certificateFingerprint = CTDCurrency.FingerPrint, version = CTDCurrency.VERSION, dependencies = CTDCurrency.DEPENDENCIES, updateJSON = CTDCurrency.updateJSON, acceptedMinecraftVersions = CTDCurrency.MCVersion)
 public class CTDCurrency {
 
 	public static final String MODID = "ctdcurrency";
 	public static final String MODNAME = "CTD Currency";
-	public static final String VERSION = "6.5";
+	public static final String VERSION = "6.5.1";
 	public static final String DEPENDENCIES = "required-after:ctdcore@[1.2,]";
 	public static final String updateJSON = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/CTD-Currency.json";
 	public static final String MCVersion = "1.12.2";
+	public static final String FingerPrint = "441b509a0f58a0ef41aca8daf1be20d96287635e";
 
 	@Instance
 	public static CTDCurrency instance = new CTDCurrency();
@@ -31,16 +34,24 @@ public class CTDCurrency {
 
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent e) {
-		this.proxy.preInit(e);
+		logger = e.getModLog();
+		logger.info("Cha-ching! CTD Currency is loading!");
+		proxy.preInit(e);
 	}
 
 	@EventHandler
 	public void init(FMLInitializationEvent e) {
-		this.proxy.init(e);
+		proxy.init(e);
 	}
 
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent e) {
-		this.proxy.postInit(e);
+		proxy.postInit(e);
+		logger.info("You win the lottery! Just kidding, but hey, CTD Currency has loaded successfully.");
+	}
+	
+	@EventHandler
+	public void onFingerprintViolation(FMLFingerprintViolationEvent e) {
+		FMLLog.warning("Invalid fingerprint detected for CTD Currency! TheMasterGeneral will not support this version!");
 	}
 }
