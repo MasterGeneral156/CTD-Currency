3b43764cb91d9e38143adb18f7d0079c799698ee
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-11-29 11:37:51 -0500

    drag and drop

--- src/main/java/com/themastergeneral/ctdcurrency/config/Config.java
+++ src/main/java/com/themastergeneral/ctdcurrency/config/Config.java
@@ -1,53 +1,52 @@
 package com.themastergeneral.ctdcurrency.config;
 
-import org.apache.logging.log4j.Level;
-
-import com.themastergeneral.ctdcurrency.Main;
-import com.themastergeneral.ctdcurrency.proxy.CommonProxy;
-
-import scala.Int;
 import net.minecraftforge.common.config.Configuration;
 
-public class Config 
-{
-    private static final String CATEGORY_GENERAL = "General";
+import org.apache.logging.log4j.Level;
 
-    // This values below you can access elsewhere in your mod:
-    public static String ModVersion = Main.VERSION+"-1.10.2";
-    public static Boolean disableJei = false;
-    public static Boolean disableBossDrops = false;
-    public static Boolean disableMobDrops = false;
-    public static int dropChance = 100;
+import com.themastergeneral.ctdcurrency.CTDCurrency;
+import com.themastergeneral.ctdcurrency.proxy.CommonProxy;
 
-    // Call this from CommonProxy.preInit(). It will create our config if it doesn't
-    // exist yet and read the values if it does exist.
-    public static void readConfig() 
-    {
-        Configuration cfg = CommonProxy.config;
-        try 
-        {
-            cfg.load();
-            initGeneralConfig(cfg);
-        } 
-        catch (Exception e1) 
-        {
-            Main.logger.log(Level.ERROR, "Problem loading config file!", e1);
-        }
-        finally 
-        {
-            if (cfg.hasChanged()) 
-            {
-                cfg.save();
-            }
-        }
-    }
-    private static void initGeneralConfig(Configuration cfg) 
-    {
-        cfg.addCustomCategoryComment(CATEGORY_GENERAL, "General configuration for the CTD Currency mod.");
-        ModVersion = cfg.getString("ModVersion", CATEGORY_GENERAL, ModVersion, "Internal. Don't need to mess with this.");
-        disableJei = cfg.getBoolean("disableJei", CATEGORY_GENERAL, disableJei, "Set to true to remove Just Enough Items integration.");
-        disableMobDrops = cfg.getBoolean("disableMobDrops", CATEGORY_GENERAL, disableMobDrops, "Set to true to stop mobs dropping currency occasionally on death.");
-        disableBossDrops = cfg.getBoolean("disableBossDrops", CATEGORY_GENERAL, disableBossDrops, "Set to true to stop Boss Mobs dropping substantial amount of currency on death.");
-        dropChance = cfg.getInt("dropChance", CATEGORY_GENERAL, dropChance, 2, 65655, "How often mobs will drop currency. Higher the number, less of a chance.");
-    }
+public class Config {
+	private static final String CATEGORY_GENERAL = "General";
+
+	public static Boolean disableBossDrops = false;
+	public static Boolean disableMobDrops = false;
+	public static int dropChance = 100;
+
+	// Call this from CommonProxy.preInit(). It will create our config if it
+	// doesn't
+	// exist yet and read the values if it does exist.
+	public static void readConfig() {
+		Configuration cfg = CommonProxy.config;
+		try {
+			cfg.load();
+			initGeneralConfig(cfg);
+		} catch (Exception e1) {
+			CTDCurrency.logger.log(Level.ERROR, "Problem loading config file!",
+					e1);
+		} finally {
+			if (cfg.hasChanged()) {
+				cfg.save();
+			}
+		}
+	}
+
+	private static void initGeneralConfig(Configuration cfg) {
+		cfg.addCustomCategoryComment(CATEGORY_GENERAL,
+				"General configuration for the CTD Currency mod.");
+		disableMobDrops = cfg
+				.getBoolean("disableMobDrops", CATEGORY_GENERAL,
+						disableMobDrops,
+						"Set to true to stop mobs dropping currency occasionally on death.");
+		disableBossDrops = cfg
+				.getBoolean(
+						"disableBossDrops",
+						CATEGORY_GENERAL,
+						disableBossDrops,
+						"Set to true to stop Boss Mobs dropping substantial amount of currency on death.");
+		dropChance = cfg
+				.getInt("dropChance", CATEGORY_GENERAL, dropChance, 2, 65655,
+						"How often mobs will drop currency. Higher the number, less of a chance.");
+	}
 }
--- src/main/java/com/themastergeneral/ctdcurrency/items/Tool.java
+++ src/main/java/com/themastergeneral/ctdcurrency/items/Tool.java
@@ -6,30 +6,29 @@ import net.minecraft.item.ItemStack;
 
 public class Tool extends Items implements ItemModelProvider {
 
-	protected Tool(String name, String modid) 
-	{
-		super(name,modid);
+	protected Tool(String name, String modid) {
+		super(name, modid);
 		this.maxStackSize = 1;
 		this.setNoRepair();
 	}
-	public boolean doesContainerItemLeaveCraftingGrid(ItemStack itemStack)
-	{
-		 return false;
+
+	public boolean doesContainerItemLeaveCraftingGrid(ItemStack itemStack) {
+		return false;
 	}
-	 @Override
-	 public boolean getShareTag()
-	 {
-		 return true;
-	 }
-	 public boolean hasContainerItem(ItemStack itemStack)
-	 {
-		 return true;
-	 }
-	 @Override
-	 public ItemStack getContainerItem(ItemStack itemStack)
-	 {
-		 ItemStack stack = itemStack.copy();
-		 this.maxStackSize = 1;
-		 return stack;
-	 }
+
+	@Override
+	public boolean getShareTag() {
+		return true;
+	}
+
+	public boolean hasContainerItem(ItemStack itemStack) {
+		return true;
+	}
+
+	@Override
+	public ItemStack getContainerItem(ItemStack itemStack) {
+		ItemStack stack = itemStack.copy();
+		this.maxStackSize = 1;
+		return stack;
+	}
 }
